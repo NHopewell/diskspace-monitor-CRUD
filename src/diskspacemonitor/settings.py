86b74b830fff08bc85f74166325a055e1c34eac2
Nick Hopewell <nicholashopewell@gmail.com>
2022-01-21 17:29:56 -0800

    include settings file for global constantsin root

---
+++ src/diskspacemonitor/settings.py
@@ -0,0 +1,7 @@
+# how close (in Gigabits) should an Agents current storage useage
+# be to its storage limit before a "close to memory limit" warning is
+# registered. Default = 10 Gigabits from upper limit.
+CLOSE_TO_STORAGE_LIMIT_TRIGGER = 10
+
+
+# LOGGING
