b72039beeea397689df62dfc0b097700e26dca4d
Nick Hopewell <nicholashopewell@gmail.com>
2022-01-22 15:02:17 -0800

    rename test_agent to fit new model name

--- tests/test_models/test_system_component.py
+++ tests/test_models/test_system_component.py
@@ -1,21 +1,21 @@
-"""test_agent.py
+"""test_system_component.py
 
-Tests the Agent model. All tests rely on the pytest fixture
-crash_dump_50, an Agent initialized with 100G of total storage
+Tests the SystemComponent model. All tests rely on the pytest fixture
+crash_dump_50, a component initialized with 100G of total storage
 and 50G of currently used storage.
 """
 import pytest
 
-from diskspacemonitor.models import Agent
+from diskspacemonitor.models import SystemComponent
 import diskspacemonitor.warn as warn
 
 
-def test_storage_limit_101_percent_invalid(crash_dump_50: Agent) -> bool:
+def test_storage_limit_101_percent_invalid(crash_dump_50: SystemComponent) -> bool:
     with pytest.raises(warn.StorageLimitOutOfRangeError):
         crash_dump_50.set_storage_limit(101)
 
 
-def test_current_storage_useage_over_limit(crash_dump_50: Agent) -> bool:
+def test_current_storage_useage_over_limit(crash_dump_50: SystemComponent) -> bool:
     with pytest.raises(warn.OverMemoryLimitError):
         crash_dump_50.set_storage_limit(90)
         crash_dump_50.set_current_storage_useage(91)
@@ -25,7 +25,7 @@ def test_current_storage_useage_over_limit(crash_dump_50: Agent) -> bool:
     "test_input,expected", [(55, 55), (30, 30), (15, 15), (90, 90), (1, 1)]
 )
 def test_current_storage_useage_change(
-    test_input: int, expected: int, crash_dump_50: Agent
+    test_input: int, expected: int, crash_dump_50: SystemComponent
 ) -> bool:
     crash_dump_50.set_current_storage_useage(test_input)
 
@@ -38,7 +38,7 @@ def test_current_storage_useage_change(
     "test_input,expected", [(55, 45), (30, 70), (15, 85), (90, 10), (1, 99)]
 )
 def test_free_storage_change(
-    test_input: int, expected: int, crash_dump_50: Agent
+    test_input: int, expected: int, crash_dump_50: SystemComponent
 ) -> bool:
     crash_dump_50.set_current_storage_useage(test_input)
 
@@ -54,7 +54,7 @@ def test_free_storage_change(
 def test_proportion_of_storage_used_change(
     test_input: int,
     expected: int,
-    crash_dump_50: Agent,
+    crash_dump_50: SystemComponent,
 ) -> bool:
     crash_dump_50.set_current_storage_useage(test_input)
 
