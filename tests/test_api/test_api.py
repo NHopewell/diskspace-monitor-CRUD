8bd009457e5d7c04fad71f24b7b6366cb6e3d0e9
Nick Hopewell <nicholashopewell@gmail.com>
2022-01-26 20:16:40 -0800

    reformat test_api.py

--- tests/test_api/test_api.py
+++ tests/test_api/test_api.py
@@ -11,6 +11,7 @@ from diskspacemonitor.main import app
 # FastAPI test client
 client = TestClient(app)
 
+
 def test_read_system_component_ok():
     response = client.get("/v1/system_components")
     assert response.status_code == 200
@@ -19,18 +20,17 @@ def test_read_system_component_ok():
 def test_post_system_component():
     response = client.post(
         "/v1/system_components",
-        json={"name": "CrashDump", "total_available_storage": 400}
+        json={"name": "CrashDump", "total_available_storage": 400},
     )
 
     assert response.status_code == 200
+
 
 def test_system_component_name_not_found():
     response = client.get("/v1/system_components/ImaginaryComponent")
 
     actual = response.json()
-    expected =  {
-            "Error": "ImaginaryComponent does not exist in the monitored system."
-        }
+    expected = {"Error": "ImaginaryComponent does not exist in the monitored system."}
 
     assert actual == expected
 
@@ -38,14 +38,11 @@ def test_system_component_name_not_found():
 def test_reject_duplicate_system_component():
     client.post(
         "/v1/system_components",
-        json={"component_name": "CrashDump", "total_available_storage": 400}
+        json={"component_name": "CrashDump", "total_available_storage": 400},
     )
     second_response = client.post(
         "/v1/system_components",
-        json={"component_name": "CrashDump", "total_available_storage": 400}
+        json={"component_name": "CrashDump", "total_available_storage": 400},
     )
 
     assert second_response.status_code == 422
-
-
-        
