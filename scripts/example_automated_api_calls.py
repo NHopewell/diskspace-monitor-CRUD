5a44f1d35e8ad2228bcad07f65cb9302d7dee865
Nick Hopewell <nicholashopewell@gmail.com>
2022-01-26 20:16:23 -0800

    reformat example api calls script

--- scripts/example_automated_api_calls.py
+++ scripts/example_automated_api_calls.py
@@ -10,67 +10,58 @@ if __name__ == "__main__":
 
     # create crash dump component
     print("Creating system component: CrashDump with 400G initial storage")
-    request_body = {
-        "name": "CrashDump",
-        "total_available_storage": 400    
-        }
+    request_body = {"name": "CrashDump", "total_available_storage": 400}
     requests.post(system_components_endpoint, json=request_body)
 
     # create versioning system
     print("Creating system component: VersioningSystem with 200G initial storage")
-    request_body = {
-        "name": "VersioningSystem",
-        "total_available_storage": 200    
-        }
+    request_body = {"name": "VersioningSystem", "total_available_storage": 200}
     requests.post(system_components_endpoint, json=request_body)
 
     # create build system
     print("Creating system component: BuildSystem with 600G initial storage")
-    request_body = {
-        "name": "BuildSystem",
-        "total_available_storage": 600    
-        }
+    request_body = {"name": "BuildSystem", "total_available_storage": 600}
     requests.post(system_components_endpoint, json=request_body)
     print()
 
-
     # read crash dump component
     print("Reading system component: CrashDump")
-    response = requests.get(os.path.join(system_components_endpoint, "CrashDump")).json()
+    response = requests.get(
+        os.path.join(system_components_endpoint, "CrashDump")
+    ).json()
     print(response)
     print()
-
 
     # update crash dump component
     print("Updating system component: CrashDump with 90 storage limit.")
-    request_body = {
-        "storage_limit": 90    
-        }
-    requests.patch(os.path.join(system_components_endpoint, "CrashDump"), json=request_body)
+    request_body = {"storage_limit": 90}
+    requests.patch(
+        os.path.join(system_components_endpoint, "CrashDump"), json=request_body
+    )
 
-    response = requests.get(os.path.join(system_components_endpoint, "CrashDump")).json()
+    response = requests.get(
+        os.path.join(system_components_endpoint, "CrashDump")
+    ).json()
     print(response)
     print()
-
 
     print("Updating system component: BuildSystem with 90 percent storage limit.")
-    request_body = {
-        "storage_limit": 90    
-        }
-    requests.patch(os.path.join(system_components_endpoint, "BuildSystem"), json=request_body)
+    request_body = {"storage_limit": 90}
+    requests.patch(
+        os.path.join(system_components_endpoint, "BuildSystem"), json=request_body
+    )
 
-    response = requests.get(os.path.join(system_components_endpoint, "BuildSystem")).json()
+    response = requests.get(
+        os.path.join(system_components_endpoint, "BuildSystem")
+    ).json()
     print(response)
     print()
-
-
 
     # list all system components
     print("Listing all system components.")
     response = requests.get(system_components_endpoint).json()
     print(response)
     print()
-
 
     # delete versioning system
     print("Deleting system component: VersioningSystem.")
@@ -83,37 +74,42 @@ if __name__ == "__main__":
     print(response)
     print()
 
-
     # test setting storage limit over 100%
     print("Testing that setting storage limit to 101 percent will fail")
-    request_body = {
-        "storage_limit": 101    
-        }
-    response = requests.patch(os.path.join(system_components_endpoint, "BuildSystem"), json=request_body)
+    request_body = {"storage_limit": 101}
+    response = requests.patch(
+        os.path.join(system_components_endpoint, "BuildSystem"), json=request_body
+    )
     print(response.json())
     print()
 
-    
     # set current storage useage over the storage limit
-    print("Setting current storage limit of CrashDump to above storage limit threshold.")
-    request_body = {
-        "current_storage_useage": 395   
-    }
-    requests.patch(os.path.join(system_components_endpoint, "CrashDump"), json=request_body)
-    response = requests.get(os.path.join(system_components_endpoint, "CrashDump")).json()
+    print(
+        "Setting current storage limit of CrashDump to above storage limit threshold."
+    )
+    request_body = {"current_storage_useage": 395}
+    requests.patch(
+        os.path.join(system_components_endpoint, "CrashDump"), json=request_body
+    )
+    response = requests.get(
+        os.path.join(system_components_endpoint, "CrashDump")
+    ).json()
     print(response)
     print()
 
     # set current storage useage over the storage limit
-    print("Setting current storage limit of BuildSytem to above storage limit threshold.")
-    request_body = {
-        "current_storage_useage": 598   
-    }
-    requests.patch(os.path.join(system_components_endpoint, "BuildSystem"), json=request_body)
-    response = requests.get(os.path.join(system_components_endpoint, "BuildSystem")).json()
+    print(
+        "Setting current storage limit of BuildSytem to above storage limit threshold."
+    )
+    request_body = {"current_storage_useage": 598}
+    requests.patch(
+        os.path.join(system_components_endpoint, "BuildSystem"), json=request_body
+    )
+    response = requests.get(
+        os.path.join(system_components_endpoint, "BuildSystem")
+    ).json()
     print(response)
     print()
-
 
     # get latest reported useage for crash dump
     print("Getting latest reported useage for CrashDump.")
@@ -123,7 +119,9 @@ if __name__ == "__main__":
 
     # get event history of events for crashdump
     print("Listing complete useage history for CrashDump.")
-    response = requests.get(os.path.join(system_events_endpoint, "CrashDump", "history")).json()
+    response = requests.get(
+        os.path.join(system_events_endpoint, "CrashDump", "history")
+    ).json()
     print(response)
     print()
 
@@ -133,15 +131,7 @@ if __name__ == "__main__":
     print(response)
     print()
 
-
     # get list of resource warnings
     print("Listing all resource warnings.")
     response = requests.get(resource_warnings_endpoint).json()
     print(response)
-    
-
-
-
-    
-
-    
